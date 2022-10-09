b31549bbdc052ee62ca8e36fd9ea06f4cfa7e0c8
Punit Jain <contactpunitjain@gmail.com>
2022-10-09 11:44:30 +0530

    dynamic handling of modal and navbar completed

--- shared.js
+++ shared.js
@@ -3,16 +3,17 @@ const modal = document.querySelector( '.modal' )
 const selectPlansButtons = document.querySelectorAll( '.plan button' )
 const toggleButton = document.querySelector( '.toggle-button' )
 const mobileNav = document.querySelector( '.mobile-nav' )
+const modalActionNo = document.querySelector( '.modal__action--negative' )
 for ( const plan of selectPlansButtons ) {
     plan.addEventListener( 'click', function () {
         modal.classList.add( 'open' )
         backdropEl.classList.add( 'open' )
     } )
 }
+
 backdropEl.addEventListener( 'click', function () {
     mobileNav.classList.remove( 'open' )
     closeModal()
-
 } )
 
 toggleButton.addEventListener( 'click', function () {
@@ -21,6 +22,14 @@ toggleButton.addEventListener( 'click', function () {
 } )
 
 function closeModal() {
-    modal.classList.remove( 'open' )
+    if ( modal ) {
+        modal.classList.remove( 'open' )
+    }
     backdropEl.classList.remove( 'open' )
+}
+
+if ( modalActionNo ) {
+    modalActionNo.addEventListener( 'click', function () {
+        closeModal()
+    } )
 }
