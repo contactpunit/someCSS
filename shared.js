cf5cf802658da621b1bcb83fcc11fcfae352c128
Punit Jain <contactpunitjain@gmail.com>
2022-10-09 09:05:15 +0530

    changed adding inline style to adding and removing classes using classList

--- shared.js
+++ shared.js
@@ -1,9 +1,26 @@
 const backdropEl = document.querySelector( '.backdrop' )
 const modal = document.querySelector( '.modal' )
 const selectPlansButtons = document.querySelectorAll( '.plan button' )
+const toggleButton = document.querySelector( '.toggle-button' )
+const mobileNav = document.querySelector( '.mobile-nav' )
 for ( const plan of selectPlansButtons ) {
     plan.addEventListener( 'click', function () {
-        modal.style.display = 'block'
-        backdropEl.style.display = 'block'
+        modal.classList.add( 'open' )
+        backdropEl.classList.add( 'open' )
     } )
+}
+backdropEl.addEventListener( 'click', function () {
+    mobileNav.classList.remove( 'open' )
+    closeModal()
+
+} )
+
+toggleButton.addEventListener( 'click', function () {
+    mobileNav.classList.add( 'open' )
+    backdropEl.classList.add( 'open' )
+} )
+
+function closeModal() {
+    modal.classList.remove( 'open' )
+    backdropEl.classList.remove( 'open' )
 }
